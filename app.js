81e3869081c77be667a00d127d4af4fbfd19f784
Hernan Moreno <hernanandres.mf@gmail.com>
2022-01-07 17:28:14 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+const {API_VERSION} =require('./config');
+
+//load routings
+//....
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//configure Header HTTP
+
+// Router Basic
+//...
+
+module.exports = app;
